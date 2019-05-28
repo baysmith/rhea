efec6a14cd722132b1e988e61a6c7cc0deddfebb
Bradley Smith <bradley@baysmith.com>
2019-05-28 10:07:47 -0700

    Allocator for a custom allocator function

--- rhea/memory.cpp
+++ rhea/memory.cpp
@@ -1,5 +1,21 @@
 #include "memory.hpp"
 
+#if USE_ALLOCATOR_FUNCTION
+namespace rhea
+{
+
+allocator_function* alloc_function;
+void* allocator_user_data;
+
+void set_allocator_function(allocator_function* allocator_f, void* user_data)
+{
+    alloc_function = allocator_f;
+    allocator_user_data = user_data;
+}
+
+} // namespace rhea
+#endif // USE_ALLOCATOR_FUNCTION
+
 #if USE_COUNTING_ALLOCATOR
 size_t rhea::counting_allocator_count;
-#endif
+#endif // USE_COUNTING_ALLOCATOR
--- rhea/memory.hpp
+++ rhea/memory.hpp
@@ -5,11 +5,47 @@
 namespace rhea
 {
 
+#if USE_ALLOCATOR_FUNCTION
+typedef void *allocator_function(void *user_data, void *ptr, size_t new_size, size_t old_size);
+extern allocator_function* alloc_function;
+extern void* allocator_user_data;
+void set_allocator_function(allocator_function* allocator_f, void *user_data);
+
+template <typename T>
+struct function_allocator {
+    typedef T value_type;
+    function_allocator() = default;
+    template <class U>
+    constexpr function_allocator(const function_allocator<U>&) noexcept
+    {
+    }
+    T* allocate(std::size_t n)
+    {
+        void* p = alloc_function(allocator_user_data, 0, n * sizeof(T), 0);
+        return (T*)p;
+    }
+    void deallocate(T* p, std::size_t n) noexcept {
+        alloc_function(allocator_user_data, p, 0, n * sizeof(T));
+    }
+};
+
+template <class T, class U>
+bool operator==(const function_allocator<T>&, const function_allocator<U>&)
+{
+    return true;
+}
+
+template <class T, class U>
+bool operator!=(const function_allocator<T>&, const function_allocator<U>&)
+{
+    return false;
+}
+#endif // USE_ALLOCATOR_FUNCTION
+
 #if USE_COUNTING_ALLOCATOR
 extern size_t counting_allocator_count;
 template <typename T>
 struct counting_allocator {
-    std::allocator<T> alloc_;
     typedef T value_type;
     counting_allocator() = default;
     template <class U>
@@ -65,13 +101,18 @@ bool operator!=(const default_allocator<T>&, const default_allocator<U>&)
 }
 
 
+#if USE_ALLOCATOR_FUNCTION
+template <typename T>
+using allocator = function_allocator<T>;
+#else
 #if USE_COUNTING_ALLOCATOR
 template <typename T>
 using allocator = counting_allocator<T>;
 #else
 template <typename T>
 using allocator = default_allocator<T>;
-#endif
+#endif // USE_COUNTING_ALLOCATOR
+#endif // USE_ALLOCATOR_FUNCTION
 
 template<typename T>
 allocator<T>& get_allocator()
